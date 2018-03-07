cfd690987790a48970c060b129bb8aac2298188b
martinbarge <m.i.barge@qmul.ac.uk>
2018-03-07 09:48:44 +0000

    Create LocalSbtSettings.scala

---
+++ LocalSbtSettings.scala
@@ -0,0 +1,7 @@
+import sbt._
+
+object LocalSbtSettings {
+ val githubBaseUrl = "https://github.com/martinbarge/tap" //Change this to your fork
+ val scaladocApiBaseUrl = "https://martinbarge.github.io/tap" //Change this to your fork
+ val dockerRepoURI = "the.url.to.publish.docker.images.to"
+}
